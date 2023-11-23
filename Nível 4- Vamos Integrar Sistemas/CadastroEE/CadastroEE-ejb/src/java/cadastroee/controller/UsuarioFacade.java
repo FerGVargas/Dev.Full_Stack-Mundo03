1a5d57b1ae4e7649e68f05807aa907c50ed68b62
Fer G. Vargas <fernandadguimaraes@hotmail.com>
2023-11-23 00:27:33 -0300

    Parte 1 Trab 3

---
+++ Nível 4- Vamos Integrar Sistemas/CadastroEE/CadastroEE-ejb/src/java/cadastroee/controller/UsuarioFacade.java
@@ -0,0 +1,25 @@
+
+package cadastroee.controller;
+
+import cadastroee.model.Usuario;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+
+@Stateless
+public class UsuarioFacade extends AbstractFacade<Usuario> implements UsuarioFacadeLocal {
+
+    @PersistenceContext(unitName = "CadastroEE-ejb")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public UsuarioFacade() {
+        super(Usuario.class);
+    }
+    
+}
